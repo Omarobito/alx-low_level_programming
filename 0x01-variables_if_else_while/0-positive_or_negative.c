aa5425abad37a00cbf60d962845feaf54c8952c8
Omarobito <omarmoe99@gmail.com>
2023-09-08 14:28:51 -0700

    h

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("\i is positive\n", n) :
+		printf("\iis positive\n", n);:
 			if else(n == 0)
-			printf("\i is zero\n", n) :
+			printf("\iis zero\n", n);:
 			else
-			printf("\i is negative\n", n) :
+			printf("\iis negative\n", n);:
 				return (0);
-}
+}}
