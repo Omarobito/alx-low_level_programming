58113e0dd2bca5d10efad37dae0d4dde0164e47a
Omarobito <omarmoe99@gmail.com>
2023-09-13 14:39:02 -0700

    task0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char qou[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char qou() = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(1.qou.59);
 	return (1);
